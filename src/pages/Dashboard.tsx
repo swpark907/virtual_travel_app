544333683f500ee0a9c571d015a6a1aae2e4a501
2022570 <seungwoo_park2@tmax.co.kr>
2023-04-18 19:47:12 +0900

    feat: 필요한 페이지 파일 추가

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface DashboardProps {}
+
+const Dashboard: React.FC<DashboardProps> = ({}) => {
+  return <div>Dashboard</div>;
+};
+export default Dashboard;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Landing: React.FC = () => {
+  return (
+    <div className="md:container bg-indigo-500 px-4 md:mx-auto">Landing</div>
+  );
+};
+
+export default Landing;
