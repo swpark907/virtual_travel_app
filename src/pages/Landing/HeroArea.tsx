2a9aecc169a8c0c80283663bcf71ef575cd61f80
swpark907 <swpark907@naver.com>
2023-05-26 15:00:30 +0900

    feat: SearchArea 리팩토링

--- src/pages/Landing/HeroArea.tsx
+++ src/pages/Landing/HeroArea.tsx
@@ -4,7 +4,7 @@ import CarouselSection from "./CarouselSection";
 
 const HeroArea: React.FC = () => {
   return (
-    <header className="w-full bg-themeColor py-4  px-4 flex">
+    <header className="w-full bg-themeColor py-4  px-4 flex md:flex-row flex-col">
       <TitleSection />
       <CarouselSection />
     </header>
--- src/pages/Landing/SearchArea.tsx
+++ src/pages/Landing/SearchArea.tsx
@@ -1,25 +1,12 @@
 import React from "react";
-import { LandingPageData } from "../../constants";
-import SearchButton from "../../assets/search-button.png";
+import RealTimeTrending from "./RealTimeTrending";
+import SearchInput from "./SearchInput";
 
 const SearchArea: React.FC = () => {
-  const {
-    searchSection: { searchPlaceholder },
-  } = LandingPageData;
   return (
-    <main className="px-20 flex justify-center w-1/2 mx-auto mt-4">
-      <div className="relative w-full">
-        <input
-          type="text"
-          placeholder={searchPlaceholder}
-          className="px-4 py-2 border-4 rounded-full w-full focus:outline-none focus:border-black"
-        />
-        <img
-          src={SearchButton}
-          alt="SearchButton"
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 w-10 h-10"
-        />
-      </div>
+    <main className="px-20 flex justify-center w-3/5 mx-auto mt-4 flex-col min-w-max">
+      <SearchInput />
+      <RealTimeTrending />
     </main>
   );
 };
